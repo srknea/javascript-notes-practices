d466445d320862520fb488a8f49e9cfc3986a2f3
Serkan ISIK <dehnas79@gmail.com>
2022-09-19 16:08:24 +0300

    dom initial

---
+++ dom/app.js
@@ -0,0 +1,43 @@
+let value;
+
+value = window;
+console.log(value);
+
+value = window.document; //DOM
+console.log(value);
+
+value = document.body;
+console.log(value);
+
+value = document.domain;
+console.log(value);
+
+value = document.URL;
+console.log(value);
+
+value = document.contentType;
+console.log(value);
+
+value = document.forms;
+console.log(value);
+
+value = document.forms[0].id;
+console.log(value);
+
+value = document.links;
+console.log(value);
+
+value = document.links[2].className;
+console.log(value);
+
+value = document.links[2].classList;
+console.log(value);
+
+value = document.images;
+console.log(value);
+
+value = document.images[0].src;
+console.log(value);
+
+value = document.images[0].getAttribute('src');
+console.log(value);
