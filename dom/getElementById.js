1a2b779b3263c7505f36a7a0c9893a0f7613a2ea
Serkan ISIK <dehnas79@gmail.com>
2022-09-20 22:07:58 +0300

    Create getElementById and querySelector

---
+++ dom/getElementById.js
@@ -0,0 +1,28 @@
+//Tek element seçiciler
+
+console.log(document.getElementById('baslik'));
+
+console.log(document.getElementById('baslik').className);
+
+
+const a = document.getElementById('baslik');
+a.style.backgroundColor = 'red';
+a.style.color = 'yellow';
+a.style.padding = '5rem';
+
+
+const b = document.getElementById('img');
+b.style.display = 'none';
+
+
+
+const c = document.getElementById('kucukBaslik');
+c.textContent = "Bu başlığın içeriği JS ile değiştirildi."
+//c.innerText = "Bu başlığın içeriği JS ile değiştirildi."
+
+const d = document.getElementById('ilkParagraf');
+console.log(d.textContent);
+
+
+const e = document.getElementById('ikinciParagraf');
+e.innerHTML = "<h1>Bu başlık innerHTML ile oluşturuldu.</h1>"
---
+++ dom/querySelector.js
@@ -0,0 +1,14 @@
+console.log(document.querySelector('p'));
+console.log(document.querySelector('#img'));
+
+console.log(document.querySelector('.liste-item'));
+console.log(document.querySelector('ul li'));
+
+console.log(document.querySelector('.link.link-google'));
+
+
+//Sadece ilk tek ve ilk çift eleman renk değiştirir. Çünkü tek secici...
+document.querySelector('li:nth-child(even)').style.color = 'blue'
+document.querySelector('li:nth-child(odd)').style.color = 'red'
+
+
