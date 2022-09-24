064010173565dbdd350e189575b2f20477eaef48
Serkan ISIK <dehnas79@gmail.com>
2022-09-24 15:11:26 +0300

    JSON initial

---
+++ JSON/app.js
@@ -0,0 +1,13 @@
+const text = '[ "Ford", "BMW", "Audi", "Fiat" ]';
+
+const myArr = JSON.parse(text); //myArr is now a array
+
+console.log(myArr);
+
+console.log("---------------------------------");
+
+const arr = ["John", "Peter", "Sally", "Jane"];
+
+const myJSON = JSON.stringify(arr); //myJSON is now a string
+
+console.log(myJSON);
